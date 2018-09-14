7be9a8c562b502a2931cde4937473f13cd88b07b
alexHooper <alexmohack@gmail.com>
2018-09-15 00:16:36 +0530

    log using all loggers

---
+++ use_loggers.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.debug("this is a debug message")
+logging.info("this is a debug message")
+logging.warning("this is a debug message")
+logging.error("this is a debug message")
+logging.critical("this is a debug message")
