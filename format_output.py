62ebeab7890c2f43eb10063e064bb2b48a4ea7d0
alexHooper <alexmohack@gmail.com>
2018-09-15 09:47:22 +0530

    log path of file

--- format_output.py
+++ format_output.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-	format='%(asctime)s - %(levelname)s - %(message)s',
+	format='%(asctime)s - %(levelname)s - %(message)s - %(pathname)s',
 	datefmt='%d-%b-%y %H:%M:%S'
 )
 logging.warning("Admin logged in")
