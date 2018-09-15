b9b5ee59f238bb6ca7ab6f9a4707edd1212c0b11
alexHooper <alexmohack@gmail.com>
2018-09-15 10:56:20 +0530

    set level for handlers

--- custom_logger.py
+++ custom_logger.py
@@ -5,3 +5,6 @@ logger = logging.getLogger(__name__)
 
 c_handler = logging.StreamHandler()
 f_handler = logging.FileHandler('file.log')
+
+c_handler.setLevel(logging.WARNING)
+f_handler.setLevel(logging.ERROR)
