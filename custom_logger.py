c0b619aef4f9abd6c130e5d93100bb52f8ad0bcb
alexHooper <alexmohack@gmail.com>
2018-09-15 11:01:29 +0530

    add formatters for handlers

--- custom_logger.py
+++ custom_logger.py
@@ -8,3 +8,6 @@ f_handler = logging.FileHandler('file.log')
 
 c_handler.setLevel(logging.WARNING)
 f_handler.setLevel(logging.ERROR)
+
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
