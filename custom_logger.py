b9ccf3daee5a4df915c88d64d6f5be0b8c03d43a
alexHooper <alexmohack@gmail.com>
2018-09-15 11:06:56 +0530

    correct error in custom logger

--- custom_logger.py
+++ custom_logger.py
@@ -13,7 +13,7 @@ c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 c_handler.setFormatter(c_format)
-f_format.setFormatter(f_format)
+f_handler.setFormatter(f_format)
 
 logger.addHandler(c_handler)
 logger.addHandler(f_handler)
